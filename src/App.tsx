538eb0e096e49d66e465e15d60e44f4990e65355
Hant <hangem422@gmail.com>
2021-08-12 12:28:14 +0900

    Function - touch scroll event for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,74 @@
 import React, { useState, useCallback, useRef, useMemo, useEffect } from 'react';
 
+import useScrollEvent from 'modules/hooks/use-scroll-event';
 import Sequence01 from 'components/sequence01';
 import animation from 'static/animation/index.json';
 import 'styles/main.scss';
 
 function App() {
+  const [scene, setScene] = useState(1); // 현재 Scene 번호
   const [width, setWidth] = useState(document.body.clientWidth); // Application Width
   const [height, setHeight] = useState(document.body.clientHeight); // Application Height
 
-  const [scene, setScene] = useState(1); // 현재 Scene 번호
-  const [progress, setProgress] = useState(0); // 현재 Scene 진행률
-  const actionFlag = useRef(false); // 앤션 진행 플래그
-  const eventFalg = useRef(false); // 이벤트 디바운스를 플래그
-
   const actionCnt = useRef<number>(0); // 현재 Action 수행중인 Sequence 갯수
-  const wheelStop = useRef<ReturnType<typeof setTimeout>>(); // 마우스 휠이 멈추면 발생하는 Timeout Event Handler
   const isMaxScene = useMemo(() => scene === animation.totalScene, [scene]); // 현재 Scene이 마지막인지 여부
   const isMinScene = useMemo(() => scene === 1, [scene]); // 현재 Scene이 처음인지 여부
+
+  const { dist, initDist, setFreeze, wheelCallback, touchStartCallback, touchMoveCallback, touchEndCallback } =
+    useScrollEvent({ wheelSensitive: animation.deletaSensitive });
 
   // 다음 Scene을 진행합니다.
   const moveNextScene = useCallback(() => {
     if (!isMaxScene) setScene(scene + 1);
-    if (progress) setProgress(0);
-  }, [isMaxScene, scene, progress]);
+    if (dist) initDist();
+  }, [isMaxScene, scene, dist]);
 
   // 이전 Scene을 진행합니다.
   const movePrevScene = useCallback(() => {
     if (!isMinScene) setScene(scene - 1);
-    if (progress) setProgress(0);
-  }, [isMinScene, scene, progress]);
+    if (dist) initDist();
+  }, [isMinScene, scene, dist]);
 
   // Sequence의 Action 실행/종료를 보고 받습니다.
-  const registAction = useCallback((regist: boolean) => {
-    if (regist) actionCnt.current += 1;
-    else if (actionCnt.current > 0) actionCnt.current -= 1;
+  const registAction = useCallback(
+    (regist: boolean) => {
+      if (regist) actionCnt.current += 1;
+      else if (actionCnt.current > 0) actionCnt.current -= 1;
 
-    if (actionCnt.current === 0) actionFlag.current = false;
-    else if (actionFlag.current === false) actionFlag.current = true;
-  }, []);
-
-  // 진행률의 변동을 반영합니다.
-  const renderProgress = useCallback(
-    (delta: number) => {
-      // 더이상 진행할 쇼트가 없으면 멈춥니다.
-      const next = progress + delta * animation.deletaSensitive;
-      if (next > 0 && isMaxScene) return;
-      if (next < 0 && isMinScene) return;
-
-      // 진행률이 일장 값 이상 도달하면 다음 Scene으로 이동합니다.
-      if (Math.abs(next) >= animation.actionStart) {
-        actionFlag.current = true;
-        if (next > 0) moveNextScene();
-        else movePrevScene();
-      } else {
-        setProgress(next);
-      }
+      // Action이 실행중인 Sequence가 하나라도 있으면 스크롤 이벤트를 중단합니다.
+      setFreeze(actionCnt.current > 0);
     },
-    [progress, isMaxScene, isMinScene],
+    [setFreeze],
   );
 
-  // Wheel의 동작을 처리합니다.
-  const wheelCallback = useCallback(
-    (e: React.WheelEvent<HTMLDivElement>) => {
-      // Flag가 있거나, 수직 변동 값이 없으면 진행하지 않습니다.
-      if (eventFalg.current || actionFlag.current) return;
-
-      window.requestAnimationFrame(() => {
-        // 마우스 움직임을 Process에 반여하고, Event Flag는 제거합니다.
-        eventFalg.current = false;
-        renderProgress(e.deltaY);
-
-        // Action 상태가 아닌데 Wheel이 멈추면 Process를 초기화합니다.
-        if (wheelStop.current) clearTimeout(wheelStop.current);
-        wheelStop.current = setTimeout(() => {
-          if (!actionFlag.current) setProgress(0);
-        }, 100);
-      });
-
-      eventFalg.current = true;
-    },
-    [renderProgress],
-  );
+  // Scroll 이동 값이 일정 값 이상 도달하면 다음 Scene으로 이동합니다.
+  useEffect(() => {
+    if (Math.abs(dist) >= animation.actionStart) {
+      if (dist > 0 && !isMaxScene) moveNextScene();
+      else if (dist < 0 && !isMinScene) movePrevScene();
+    }
+  }, [dist]);
 
   // Window 사이즈가 조정되면 Sequence Container들을 resize합니다.
-  const resize = useCallback(() => {
+  const resizeCallback = useCallback(() => {
     setWidth(document.body.clientWidth);
     setHeight(document.body.clientHeight);
   }, []);
 
   useEffect(() => {
-    window.addEventListener('resize', resize);
-    return () => window.removeEventListener('resize', resize);
-  }, [resize]);
+    window.addEventListener('resize', resizeCallback);
+    return () => window.removeEventListener('resize', resizeCallback);
+  }, [resizeCallback]);
 
   return (
-    <div className="App" onWheel={wheelCallback}>
-      <Sequence01 scene={scene} progress={progress} appWidth={width} appHeight={height} registAction={registAction} />
+    <div
+      className="App"
+      onWheel={wheelCallback}
+      onTouchStart={touchStartCallback}
+      onTouchMove={touchMoveCallback}
+      onTouchEnd={touchEndCallback}
+    >
+      <Sequence01 scene={scene} progress={dist} appWidth={width} appHeight={height} registAction={registAction} />
     </div>
   );
 }
---
+++ src/modules/hooks/use-scroll-event.ts
@@ -0,0 +1,80 @@
+import React, { useState, useRef, useCallback } from 'react';
+
+interface UseScrollEventProps {
+  wheelSensitive: number;
+}
+
+function UseScrollEvent({ wheelSensitive }: UseScrollEventProps) {
+  const [dist, setDist] = useState<number>(0); // Scroll Distance
+  const [touchY, setTouchY] = useState<number | null>(null); // Touch Start Point
+
+  const eventFlag = useRef(false); // 이벤트 디바운스를 플래그
+  const freezeFlag = useRef(false); // 동작 멈춤 플래그
+  const eventStopOut = useRef<ReturnType<typeof setTimeout>>(); // 스크롤 이벤트가 멈추면 발생하는 Timeout Event Handler
+
+  // Dist 값을 0으로 초기화합니다.
+  const initDist = useCallback(() => {
+    setDist(0);
+  }, []);
+
+  // Hook 외부에서 동작 멈춤 제어
+  const setFreeze = useCallback((freeze: boolean) => {
+    if (freeze !== freezeFlag.current) freezeFlag.current = freeze;
+  }, []);
+
+  // 스크롤 이벤트 동작을 처리합니다.
+  const eventHandler = useCallback((delta: number) => {
+    // 선언된 Flag가 있으면 동작하지 않습니다.
+    if (eventFlag.current || freezeFlag.current) return;
+    eventFlag.current = true;
+
+    window.requestAnimationFrame(() => {
+      // 스크롤 움직임을 Dist에 반영하고, Event Flag는 제거합니다.
+      eventFlag.current = false;
+      setDist(delta);
+
+      // Freeze 상태가 아닌데 Scroll Event가 멈추면 Dist를 초기화합니다.
+      if (eventStopOut.current) clearTimeout(eventStopOut.current);
+      eventStopOut.current = setTimeout(() => {
+        if (!freezeFlag.current) initDist();
+      }, 100);
+    });
+  }, []);
+
+  // Wheel Event의 동작을 처리합니다.
+  const wheelCallback = useCallback(
+    (e: React.WheelEvent<HTMLDivElement>) => {
+      eventHandler(e.deltaY + dist * wheelSensitive);
+    },
+    [dist, wheelSensitive],
+  );
+
+  // Touch Start Event의 동작을 처리합니다.
+  const touchStartCallback = useCallback((e: React.TouchEvent<HTMLDivElement>) => {
+    if (e.touches.length > 1) return; // 한손가락 터치만 허용합니다.
+    setTouchY(e.touches[0].clientY);
+  }, []);
+
+  // Touch Move Event의 동작을 처리힙니다.
+  const touchMoveCallback = useCallback(
+    (e: React.TouchEvent<HTMLDivElement>) => {
+      if (typeof touchY === 'number') eventHandler(touchY - e.touches[0].clientY);
+    },
+    [touchY],
+  );
+
+  // Touch End Event의 동작을 처리합니다.
+  const touchEndCallback = useCallback(() => setTouchY(0), []);
+
+  return {
+    dist,
+    initDist,
+    setFreeze,
+    wheelCallback,
+    touchStartCallback,
+    touchMoveCallback,
+    touchEndCallback,
+  };
+}
+
+export default UseScrollEvent;
